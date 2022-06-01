a7bad1e3bcd698928bc920edc56cde68ef138674
panthersinsights-fla <102759828+panthersinsights-fla@users.noreply.github.com>
2022-06-01 14:06:25 -0400

    updated init

--- catnip/__init__.py
+++ catnip/__init__.py
@@ -1,9 +1,9 @@
-from fla_archtics import FLA_Archtics
-from fla_email import FLA_Email
-from fla_helpers import FLA_Helpers
-from fla_redshift import FLA_Redshift
-from fla_sharepoint import Fla_Sharepoint
-from my_hockey_game import MyHockeyGame
+from catnip.fla_archtics import FLA_Archtics
+from catnip.fla_email import FLA_Email
+from catnip.fla_helpers import FLA_Helpers
+from catnip.fla_redshift import FLA_Redshift
+from catnip.fla_sharepoint import Fla_Sharepoint
+from catnip.my_hockey_game import MyHockeyGame
 
 
 __all__ =[
