b5e32cb5bc31db999a675027c0acd621fc1fcc96
panthersinsights-fla <102759828+panthersinsights-fla@users.noreply.github.com>
2022-06-01 11:31:45 -0400

    added dat all thang

--- catnip/__init__.py
+++ catnip/__init__.py
@@ -4,3 +4,13 @@ from fla_helpers import FLA_Helpers
 from fla_redshift import FLA_Redshift
 from fla_sharepoint import Fla_Sharepoint
 from my_hockey_game import MyHockeyGame
+
+
+__all__ =[
+    "FLA_Archtics",
+    "FLA_Email",
+    "FLA_Helpers",
+    "FLA_Redshift",
+    "Fla_Sharepoint",
+    "MyHockeyGame"
+]
