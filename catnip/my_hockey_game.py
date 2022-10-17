b788271be0daa05b4692634c076fdde7a118541c
mcgoyen-fla <mcgoyen@floridapanthers.com>
2022-10-17 10:24:04 -0400

    my hockey game test

--- catnip/my_hockey_game.py
+++ catnip/my_hockey_game.py
@@ -28,8 +28,8 @@ class MyHockeyGame:
         FROM 
             [DBA].v_event e 
         WHERE 
-            CAST(e.event_date AS DATE) = CAST(GETDATE() AS DATE)
-            --CAST(e.event_date AS DATE) = CAST(DATEADD(DAY, 1, CAST(GETDATE() AS DATE)) AS DATE)
+            --CAST(e.event_date AS DATE) = CAST(GETDATE() AS DATE)
+            CAST(e.event_date AS DATE) = CAST(DATEADD(DAY, 2, CAST(GETDATE() AS DATE)) AS DATE)
             AND e.minor_category IN ('NHL PRO HOCKEY', 'SPORTS:NHL PRO HOCKEY')
             AND e.event_name NOT LIKE '%TEST%'
     '''
