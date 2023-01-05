5d30c54defd373acec03714920187ffdc83f06aa
mcgoyen-fla <mcgoyen@floridapanthers.com>
2023-01-05 14:16:02 -0500

    MyHockeyGame current date format

--- catnip/my_hockey_game.py
+++ catnip/my_hockey_game.py
@@ -18,7 +18,7 @@ class MyHockeyGame:
     current_datetime : datetime = datetime.now() - timedelta(hours = 5)
     current_time_formatted : str = datetime.strftime(datetime.now() - timedelta(hours = 5), '%#I:%M %p')
     current_time : datetime.time = (datetime.now() - timedelta(hours = 5)).time()
-    current_date : str = datetime.strftime(datetime.now() - timedelta(hours = 5), '%#m-%#d-%y')
+    current_date : str = datetime.strftime(datetime.now() - timedelta(hours = 5), '%-m-%-d-%y')
     event_names: List = field(default_factory=list)
 
     sql_select_statement : str = '''
