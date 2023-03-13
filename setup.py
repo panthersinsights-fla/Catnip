d9a72a006a2aba67e6c02801dccafed5f916b81f
mcgoyen-fla <mcgoyen@floridapanthers.com>
2023-03-13 11:19:32 -0400

    MyHockeyGame

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name = "catnip",
-    version = "0.0.689",
+    version = "0.0.690",
     author = "Panthers Insights",
     author_email = "panthersinsights@floridapanthers.com",
     description = "Facilitating repeatable internal code",
