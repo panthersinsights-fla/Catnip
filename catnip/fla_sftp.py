6c9758bcf969dfe6b8b228a71d0412b1e6fc03f8
panthersinsights-fla <panthersinsights@floridapanthers.com>
2023-04-07 12:47:52 -0400

    fla sftp comments

--- catnip/fla_sftp.py
+++ catnip/fla_sftp.py
@@ -18,35 +18,49 @@ class FLA_Sftp:
 
     def _create_connection(self) -> Optional[SFTPClient]:
 
+        ## Establish transport object
         transport = Transport((self.host, self.port))
         transport.connect(username = self.username, password = self.password)
 
+        ## Create SFTP connection object
         connection = SFTPClient.from_transport(transport)        
 
         return connection
 
+
     def get_all_filenames_in_directory(self) -> List:
 
+        ## Create connection
         conn = self._create_connection()
 
-        return conn.listdir(self.remote_path)
+        ## Get filenames
+        filenames = conn.listdir(self.remote_path)
+        
+        return filenames
+
 
     def file_exists(self, conn: SFTPClient) -> bool:
 
+        ## Check file existence
         try:
             conn.stat(self.remote_path)
             return True
 
+        ## Else return error message
         except SFTPError as e:
             print(f"The specified file on this '{self.remote_path}' remote_path does not exist.")
             return False
 
+
     def download_csv(self, separator: str = ",") -> pd.DataFrame:
 
+        ## Create connection
         conn = self._create_connection()
 
+        ## Initialize dataframe
         df = pd.DataFrame()
 
+        ## Download csv as dataframe
         if self.file_exists(conn):
             with conn.open(self.remote_path) as file:
                 file.prefetch()
@@ -54,10 +68,13 @@ class FLA_Sftp:
 
         return df
 
+
     def upload_csv(self, df: pd.DataFrame) -> None:
 
+        ## Create connection
         conn = self._create_connection()
 
+        ## Upload csv
         with conn.open(self.remote_path, "w") as file:
             file.write(df.to_csv(index = False))
 
